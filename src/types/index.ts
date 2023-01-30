d815292102b931133e7ff8301c6719cd6114eeb7
Gustavo <gustavoaugustocar@hotmail.com>
2023-01-30 20:31:36 -0300

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,8 @@
-import React from 'react';
+import { ReactNode } from 'react';
 
 export interface IFinance {
   id: string | number;
 }
 
-export type WithChildren<T = {}> = T & { children: React.ReactNode };
+export type WithChildren<T = {}> = T & { children: ReactNode };
 export type LooseAutoComplete<T extends string> = T | Omit<string, T>;

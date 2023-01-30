f5ef33fddb1bc92f2bba6bee1982481d36cf6926
Gustavo <gustavoaugustocar@hotmail.com>
2023-01-30 20:39:41 -0300

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,5 +4,5 @@ export interface IFinance {
   id: string | number;
 }
 
-export type WithChildren<T = {}> = T & { children: ReactNode };
+export type WithChildren<T = {}> = T & { children?: ReactNode };
 export type LooseAutoComplete<T extends string> = T | Omit<string, T>;

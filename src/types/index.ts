3a55292eafb060c254274bbfc3d6fc4eddc1508d
Gustavo <gustavoaugustocar@hotmail.com>
2023-01-30 21:01:48 -0300

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,9 @@ export interface IFinance {
 }
 
 export type FCWithChildren<T = {}, Required extends boolean = false> = FC<
-  T & Required extends true ? { children: ReactNode } : { children?: ReactNode }
+  Required extends true
+    ? T & { children: ReactNode }
+    : T & { children?: ReactNode }
 >;
 
 export type LooseAutoComplete<T extends string> = T | Omit<string, T>;

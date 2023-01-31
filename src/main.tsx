c02a0e855ce5764e6cb8725474870394529ee6c1
Gustavo <gustavoaugustocar@hotmail.com>
2023-01-31 08:41:54 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <App />
 );

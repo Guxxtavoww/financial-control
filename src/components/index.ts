d7fb29a4d33e714963e5b70e76e74e8f83bff448
Gustavo <gustavoaugustocar@hotmail.com>
2023-01-31 21:07:23 -0300

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as PageContainer } from './layout/PageContainer';
 export { default as Input } from './tools/Input';
+export { default as InfoCard } from './layout/InfoCard';
 export { default as ComposeProviders } from './tools/ComposeProviders';
 

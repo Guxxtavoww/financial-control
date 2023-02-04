dd2bf5af6a5c48e653fbae5c496a1009157120ba
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-04 12:21:02 -0300

    update

--- src/components/tools/Input/index.tsx
+++ src/components/tools/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useId, useRef } from 'react';
+import React, { useId, useMemo, useRef } from 'react';
 import { useField } from '@unform/core';
 
 import { IInputProps } from '@/types';
@@ -14,9 +14,9 @@ const Input: React.FC<IInputProps> = ({
 }) => {
   const inputId = useId();
   const inputRef = useRef<HTMLInputElement>(null);
+  const uniqueId = useMemo(() => `${name}${inputId}`, [name, inputId]);
   const { fieldName, registerField, clearError, defaultValue } = useField(name);
 
-  const uniqueId = `${name}${inputId}`;
   const path =
     rest.type === 'checkbox' || rest.type === 'radio' ? 'checked' : 'value';
 

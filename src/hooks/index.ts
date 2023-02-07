2ab7b3294bfb7b5a01eb9a6bfb52aac09a493928
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-07 08:19:12 -0300

    update

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
+export { default as useClickOutside } from './useClickOutside';
 export { default as usePersitedState } from './usePersitedState';
 export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';

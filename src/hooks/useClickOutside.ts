79d57506822d1394feb8b16a4249d5d6b7d767b5
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-07 08:09:53 -0300

    update

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,26 @@
+import { useState, useCallback, RefObject } from 'react';
+
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
+
+function useClickOutside(currentRef: RefObject<HTMLElement>): boolean {
+  const [isClickedOutside, setIsClickedOutside] = useState<boolean>(false);
+
+  const handleDocumentClick = useCallback(
+    (e: MouseEvent) => {
+      const clickedOutside = !currentRef.current?.contains(e.target as Node);
+
+      return setIsClickedOutside(clickedOutside);
+    },
+    [currentRef]
+  );
+
+  useIsomorphicLayoutEffect(() => {
+    document.addEventListener('mousedown', handleDocumentClick);
+
+    return () => document.removeEventListener('mousedown', handleDocumentClick);
+  }, [handleDocumentClick]);
+
+  return isClickedOutside;
+}
+
+export default useClickOutside;

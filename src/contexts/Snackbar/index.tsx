592a24662b23dae48e18f15154d75944d53f5edb
Gustavo <galmeida@pdasolucoes.com.br>
2023-02-08 11:51:46 -0300

    update

--- src/contexts/Snackbar/index.tsx
+++ src/contexts/Snackbar/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Close as CloseIcon } from '@material-ui/icons';

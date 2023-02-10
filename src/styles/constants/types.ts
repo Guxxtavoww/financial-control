5ce73b7fd757b5cf43a1fd80ecb679eca4b3e820
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-10 14:58:12 -0300

    update

--- src/styles/constants/types.ts
+++ src/styles/constants/types.ts
@@ -1,5 +1,9 @@
 export type Themes = 'light' | 'dark';
 
+type ConstantsModule = typeof import('./index');
+
+export type Constants = ConstantsModule[keyof ConstantsModule];
+
 export interface IThemeColors {
   greenBg: string;
   mainBg: string;

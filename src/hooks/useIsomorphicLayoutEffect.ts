35a420e21b6bad781bbf1de6664c3ccabb5d573d
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-10 16:59:24 -0300

    update

--- src/hooks/useIsomorphicLayoutEffect.ts
+++ src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,6 +1,6 @@
 import { useLayoutEffect, useEffect } from 'react';
 
-const useIsomorphicLayoutEffect =
+const useIsomorphicLayoutEffect: typeof useEffect | typeof useLayoutEffect =
   typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
 export default useIsomorphicLayoutEffect;

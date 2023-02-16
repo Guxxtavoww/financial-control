caa933b77b1e6ec99c7339643f1976c676cd5da1
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2023-02-15 21:12:19 -0300

    update

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,23 +1,47 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import axios, { Method } from 'axios';
 
 import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
-function useRequest<T>(endpoint: string, method: Method) {
+interface HookResponse<T> {
+  data: T | null;
+  isLoading: boolean;
+  errorMessage: string | undefined;
+}
+
+function useRequest<T>(endpoint: string, method: Method): HookResponse<T> {
   const [data, setData] = useState<T | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string>();
 
   useIsomorphicLayoutEffect(() => {
     const abortController = new AbortController();
-
-    setIsLoading(true);
 
     const axiosInstance = axios.create({
       baseURL: 'vem do .env',
       signal: abortController.signal,
     });
 
-    axios.request<T>({ method,  })
+    setIsLoading(true);
+
+    axiosInstance
+      .request<T>({ method, url: endpoint })
+      .then(res => {
+        setData(res.data);
+        setErrorMessage(undefined);
+      })
+      .catch(err => {
+        setData(null);
+        setErrorMessage(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => abortController.abort();
   }, [endpoint, method]);
+
+  return { data, isLoading, errorMessage };
 }
+
+export default useRequest;
